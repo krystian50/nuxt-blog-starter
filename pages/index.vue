b0e49ca15073d0328f749bdcd2a488f9748f612c
Krystian Fras <krystian@fras.me>
2020-03-12 22:44:15 +0100

    add basic homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,57 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card title="Free" icon="github-circle">
-        Open source on
-        <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
-
-      <card title="Responsive" icon="cellphone-link">
-        <b class="has-text-grey">
-          Every
-        </b>
-        component is responsive
-      </card>
-
-      <card title="Modern" icon="alert-decagram">
-        Built with
-        <a href="https://vuejs.org/">
-          Vue.js
-        </a>
-        and
-        <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card title="Lightweight" icon="arrange-bring-to-front">
-        No other internal dependency
-      </card>
+  <div>
+    <div class="hero is-primary">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">
+            Witaj na blogu!
+          </h1>
+          <span class="subtitle">
+            Dostępne artykuły poniżej
+          </span>
+        </div>
+      </div>
     </div>
-  </section>
+
+    <main class="section">
+      <div class="tile is-ancestor">
+        <div class="tile is-parent">
+          <article class="tile is-child box">
+            <!-- Add products here -->
+          </article>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import Card from '~/components/Card'
-
+// remove when will be used
+// eslint-disable-next-line no-unused-vars
+const getProducts = () => {
+  return Promise.resolve([
+    {
+      id: 'post0',
+      name: 'Kurs NuxtJS - część 1'
+    },
+    {
+      id: 'post1',
+      name: 'Kurs NuxtJS - część 2'
+    },
+    {
+      id: 'post2',
+      name: 'Kurs NuxtJS - część 3'
+    },
+    {
+      id: 'post3',
+      name: 'Kurs NuxtJS - część 4'
+    }
+  ])
+}
 export default {
   name: 'HomePage',
-
-  components: {
-    Card
+  asyncData() {
+    return {}
   }
 }
 </script>
