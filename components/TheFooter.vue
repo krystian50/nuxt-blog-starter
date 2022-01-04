881a51704fff9b77e09ae758824632ad74a4aeda
Krystian Fras <krystian@fras.me>
2022-01-04 15:29:08 +0100

    Update TheFooter.vue

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div class="content has-text-centered">
       <p>
-        <strong>MasteringVueJS</strong> by
+        <strong>Blog</strong> by
         <a href="https://devmeetings.org">Devmeetings</a>. The source code is
         licensed
         <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
